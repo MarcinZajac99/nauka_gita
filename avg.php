15f8e6931263ed0d1d6c6bd441c33af4246b78f3
MarcinZajac99 <wrx74569@student.wsb.wroclaw.pl>
2022-07-08 18:42:22 +0200

    Inne wagi 5=>7, 3=>1

--- avg.php
+++ avg.php
@@ -1,1 +1,18 @@
-114
+<?php
+function srednia()
+{
+    $liczby = func_get_args();
+    return array_sum($liczby)/func_num_args();
+}
+
+echo srednia(1, 2, 5, 3, 4, 2, 2, 1, 3);
+$oceny = array(1,2,3,4,5);
+$wagi = array(7,4,1,2,1);
+$sumaWazona = 0;
+foreach( $oceny as $klucz => $ocena ){
+  $sumaWazona +=  $ocena * $wagi[$klucz];
+  $sumaWag += $wagi[$klucz];
+}
+ 
+$wynik = $sumaWazona / $sumaWag;
+?>
